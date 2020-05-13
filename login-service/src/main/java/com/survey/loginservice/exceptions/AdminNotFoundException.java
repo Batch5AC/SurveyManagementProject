71d617e0d31f29416e8fdf8c0f4b2d4570ce8a72
Sree Pooja <sreepoojav@gmail.com>
2020-05-13 18:53:06 +0530

    bootstrap properties

--- login-service/src/main/java/com/survey/loginservice/exceptions/AdminNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.survey.loginservice.exceptions;
-
-public class AdminNotFoundException extends RuntimeException {
- 
-	
-	public AdminNotFoundException(String eId) {
-		super("could not find User " + eId);
-	}
-}
